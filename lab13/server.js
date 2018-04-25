5e385999d8d0a62553eadbef67ad5618767d86d3
ROHAN HUTCHEON (1602725) <1602725@rgu.ac.uk>
2018-04-25 12:38:01 +0100

    9

--- lab13/server.js
+++ lab13/server.js
@@ -5,3 +5,13 @@ app.get('/', function(req, res){
  res.send("Hello world! by express”);
 });
 app.listen(8080);
+
+
+app.get('/', function(req, res){
+ res.send("Hello world! by express”);
+});
+
+
+app.get('/test', function(req, res){
+ res.send("this is route 2");
+});
